aaac5a0b82f0b62e7eb6d9ce986c7ad16ccdb57d
Lucas GILHARD <l.gilhard@gmail.com>
2019-02-15 20:06:15 +0100

    build(): add dependencies
    @angular/material, @angular/animations, @angular/cdk: initialized in app.module.ts, global.scss, index.html, main.ts
    @ionic/storage: initialized in app.module.ts
    hammer.js: required by material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +15,14 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),
+
+    AppRoutingModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
