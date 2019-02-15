ebdc633ec6d647a132713d361ef34e8710683755
Lucas GILHARD <l.gilhard@gmail.com>
2019-02-15 23:06:50 +0100

    feat(AppModule): initialize fixer & units services;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { UnitsService } from './services/units.service';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { FixerService } from './services/fixer.service';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +14,9 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private unitsSrv: UnitsService,
+    private fixerSrv: FixerService
   ) {
     this.initializeApp();
   }
@@ -21,6 +25,8 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.fixerSrv.init();
+      this.unitsSrv.init();
     });
   }
 }
