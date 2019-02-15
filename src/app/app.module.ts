c68430050e5c35dbfebd5eb895af879d00c29a41
Lucas GILHARD <l.gilhard@gmail.com>
2019-02-15 23:04:22 +0100

    feat(FixerService): add init & convert functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -20,6 +21,7 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     IonicModule.forRoot(),
     IonicStorageModule.forRoot(),
+    HttpClientModule,
 
     AppRoutingModule
   ],
